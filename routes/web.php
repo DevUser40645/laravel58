12837299299ce5f93027d512474150f00e5e5ee4
DevUser40645 <test40645@gmail.com>
2020-11-11 16:41:17 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+//Route::resource('rest', 'RestTestController')->names('restTest');
+
+//Админка Блога
+$groupData = [
+	'namespace' => 'Blog\Admin',
+	'prefix' => 'admin/blog',
+];
+Route::group($groupData, function() {
+	//BlogCategory
+	$methods = ['index', 'update', 'create', 'edit', 'store',];
+	Route::resource('categories', 'CategoryController')
+		->only($methods)
+		->names('blog.admin.categories');
+
+	//BlogPost
+    Route::resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
+});
